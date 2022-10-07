fdeb1ce6b880e4838e4b3f17a7ea2c5450717ed7
Zhenrui Chen <zc2569@columbia.edu>
2022-10-07 03:50:17 +0000

    part 1 complete

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -7,7 +7,9 @@ class MoviesController < ApplicationController
   end
 
   def index
-    @movies = Movie.all
+    @movies = Movie.with_ratings(params[:ratings])
+    @all_ratings = ['G','PG','PG-13','R']
+    @ratings_to_show = []
   end
 
   def new
